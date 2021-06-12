6d28356862b805583bacf87fa8a2b4f52b4ebf48
Tanja Pyykönen <tanja.k.pyykonen@gmail.com>
2021-06-12 15:37:26 +0300

    Add exercise 03.1

---
+++ Exercise_03.1.php
@@ -0,0 +1,19 @@
+/* Kopioi ensin alla annettu, syntaksivirheitä sisältävä, lähdekoodi alapuolen tekstilaatikkoon. Paina tämän
+jälkeen vastaa-nappulaa. Automaattinen tarkastus- ja palautejärjestelmä kertoo sinulle tämän jälkeen missä ja
+millainen virhe koodissa on. Voit myös katsoa, mitä PHP-tulkki lähdekoodin virheestä sanoo. Etsi annetusta koodista
+virheet sekä korjaa ne niin, että lähdekoodi ei sisällä syntaksivirheitä ja että ohjelma toimii esimerkkitulostuksen
+mukaisesti.
+
+?php
+    echo Tämä ohjelma sisältää virheitä.\n"
+    ech "Tehtävänäsi on korjata.";
+?>
+
+Example output:
+Tämä ohjelma sisältää virheitä.
+Tehtävänäsi on korjata virheet. */
+
+<?php
+	echo "Tämä ohjelma sisältää virheitä.\n";
+	echo "Tehtävänäsi on korjata virheet.";
+?>
