31f284aa9aaaea00cb369d6fc5c63f1dc4175eff
Tanja Pyykönen <tanja.k.pyykonen@gmail.com>
2021-06-12 15:46:27 +0300

    Add exercise 04.1

---
+++ Exercise_04.1.php
@@ -0,0 +1,16 @@
+/* Kirjoita PHP-skripti, joka tulostaa itseisarvon alla olevaan lomakkeeseen annetusta luvusta.
+
+<form action="itseisarvo.php" method="get">
+Luku: <input type="text" name="luku">
+<input type="submit" value="Lähetä">
+</form>
+
+Example output:
+Luvun -3 itseisarvo on 3 */
+
+<?php
+	$luku = $_GET["luku"];
+	$itseisarvo = abs($luku);
+
+    echo "Luvun $luku itseisarvo on $itseisarvo";
+?>
