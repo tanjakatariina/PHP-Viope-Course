a6441118fdea732054b7bcc14cbf40b272031b92
Tanja Pyykönen <tanja.k.pyykonen@gmail.com>
2021-06-12 15:35:26 +0300

    Add exercise 02.1

---
+++ Exercise_02.1.php
@@ -0,0 +1,16 @@
+/* Tämän tehtävän tarkoituksena on tutustuttaa sinut PHP-kielisten ohjelmien kirjoittamiseen. Alapuolella näet
+PHP-kielisen ohjelman. Tehtäväsi on kirjoittaa ohjelma uudelleen. Seuraavassa luvussa opit, miten ohjelma toimii
+ja mitä se tekee.
+
+<?php
+    echo "Tämä on ensimmäinen ohjelma.";
+    echo "Tästä se ohjelmoinnin opiskelu alkaa!";
+?>
+
+Example output:
+Tämä on ensimmäinen ohjelma.Tästä se ohjelmoinnin opiskelu alkaa! */
+
+<?php
+	echo "Tämä on ensimmäinen ohjelma.";
+	echo "Tästä se ohjelmoinnin opiskelu alkaa!";
+?>
