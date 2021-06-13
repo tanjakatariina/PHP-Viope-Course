58d3d63531a895acc5584fe5eb984ebf87afe93b
Tanja Pyykönen <tanja.k.pyykonen@gmail.com>
2021-06-13 19:18:16 +0300

    Add exercise 05.1

---
+++ Exercise_05.1.php
@@ -0,0 +1,38 @@
+/* Muuta taulukon taulu alkioita niin, että ohjelma tulostaa kuten esimerkkitulostus. Tehtävässä array-sanan käyttö
+on kielletty eli tehtävänäsi ei ole tehdä uutta taulukkoa, vaan muuttaa olemassa olevan taulukon alkioita.
+Tekstikenttään kirjoitetaan vain puuttuvat lauseet, ei koko ohjelmaa!
+
+Täydennettävä ohjelma:
+
+<?php
+
+    $taulu = array("Ville Veikko" => "Koodaajapolku 2",
+    "Matti Meikäläinen" => "Opiskelijantie 5",
+    "Taneli Teräs" => "Teräskuja 5");
+
+    // Sinun koodi tulee tähän
+
+    print_r($taulu);
+?>
+
+Example output:
+Array
+(
+    [Ville Veikko] => Koodaajapolku 10
+    [Matti Meikäläinen] => Opiskelijantie 5
+    [Taneli Teräs] => Tanelinpolku 5
+    [Maija Meikäläinen] => Opiskelijantie 5
+) */
+
+<?php
+
+    $taulu = array("Ville Veikko" => "Koodaajapolku 2",
+           "Matti Meikäläinen" => "Opiskelijantie 5",
+           "Taneli Teräs" => "Teräskuja 5");
+
+	$taulu["Ville Veikko"]  = "Koodaajapolku 10";
+    $taulu["Taneli Teräs"] = "Tanelinpolku 5";
+    $taulu["Maija Meikäläinen"] = "Opiskelijantie 5";
+
+    print_r($taulu);
+?>
