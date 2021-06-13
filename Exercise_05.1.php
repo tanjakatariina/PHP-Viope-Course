e38027619f9972695d2b6b7a8134e51066094346
Tanja Pyykönen <77980643+tanjakatariina@users.noreply.github.com>
2021-06-13 19:19:18 +0300

    Update Exercise_05.1.php

--- Exercise_05.1.php
+++ Exercise_05.1.php
@@ -25,12 +25,11 @@ Array
 ) */
 
 <?php
-
     $taulu = array("Ville Veikko" => "Koodaajapolku 2",
            "Matti Meikäläinen" => "Opiskelijantie 5",
            "Taneli Teräs" => "Teräskuja 5");
 
-	$taulu["Ville Veikko"]  = "Koodaajapolku 10";
+    $taulu["Ville Veikko"]  = "Koodaajapolku 10";
     $taulu["Taneli Teräs"] = "Tanelinpolku 5";
     $taulu["Maija Meikäläinen"] = "Opiskelijantie 5";
 
