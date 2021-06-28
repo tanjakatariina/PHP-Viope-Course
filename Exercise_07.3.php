28704bcbfb79e48594272e4c8e66f938bc266494
Tanja Pyykönen <tanja.k.pyykonen@gmail.com>
2021-06-28 12:23:40 +0300

    Add exercise 07.3

---
+++ Exercise_07.3.php
@@ -0,0 +1,33 @@
+/* Tehtävänäsi on täydentää alla olevaa ohjelmaa, niin että ohjelma tulostaa kuten esimerkkitulostus. Ohjelmasta
+puuttuu vain yksi funktio, joka sinun pitää kirjoittaa. Sinun ei pidä kirjoittaa palautuskenttään siis mitään
+muuta koodia kuin tuo puuttuva funktio.
+
+
+Täydennettävä ohjelma:
+
+<?php
+
+// Sinun koodi tulee tähän
+
+$merkkijono = "aluksi\n";
+uusiarvo($merkkijono);
+echo "Merkkijono lopuksi: $merkkijono\n";
+
+?>
+
+Example output:
+Merkkijono aluksi: aluksi
+Merkkijono lopuksi: Uusi merkkijono */
+
+<?php
+
+	function uusiarvo(&$viiteparametri) {
+        echo "Merkkijono aluksi: $viiteparametri";
+        $viiteparametri = "Uusi merkkijono";
+    }
+
+    $merkkijono = "aluksi\n";
+    uusiarvo($merkkijono);
+    echo "Merkkijono lopuksi: $merkkijono\n";
+
+?>
